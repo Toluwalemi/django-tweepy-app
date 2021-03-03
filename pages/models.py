416dbbe81ce4668380b211b06f866ba08a817e98
t.oluwadare <t.oluwadare@innovectives.com>
2021-03-03 13:34:20 +0100

    feature: add models

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,40 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+
+class Link(models.Model):
+    """
+    A Model to store a tweet link in case a tweet has more than one link
+    """
+    tweet_link = models.URLField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = "Links"
+
+
+class Tip(models.Model):
+    """
+    A Model to store tweet details from Daily Python Tip
+    """
+    tweep = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tips')
+    python_tip = models.CharField(max_length=280)
+    link = models.ForeignKey(Link, on_delete=models.CASCADE,
+                             related_name='tips_link')  # e.g logging = Tip.objects.get(name='logging') ->
+    # logging.tips_link.all()
+    posted_by = models.CharField(max_length=20)
+    is_published = models.BooleanField(default=True)
+    likes = models.IntegerField(blank=True, null=True)  # allow empty values for db and forms
+    retweets = models.IntegerField(blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = "Tips"
+        ordering = ['-retweets', '-likes']
+
+    def __str__(self):
+        return self.python_tip
