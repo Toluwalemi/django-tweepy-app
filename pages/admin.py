6c89f9b25223efdc14c0a4e9a0e5608853cb3c54
t.oluwadare <t.oluwadare@innovectives.com>
2021-03-04 01:08:34 +0100

    feature: update models

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
 # Register your models here.
+from pages.models import Link, Tip
+
+
+@admin.register(Tip)
+class TipAdmin(admin.ModelAdmin):
+    list_display = ['timestamp', 'python_tip', 'posted_by', 'is_published', 'likes', 'retweets']
+
+
+class LinkAdmin(admin.ModelAdmin):
+    list_display = ['tip_id', 'tweet_link']
+
+    # def view_tweets_link(self, obj):
+    #     count = obj.links.count()
+    #     url = (reverse("admin:pages_link_changelist")
+    #            + "?")
+    #     + urlencode({""})
+    #     return formate_html('<a href="{}'>{} Links)
+
+
+admin.site.register(Link, LinkAdmin)
--- pages/models.py
+++ pages/models.py
@@ -4,27 +4,13 @@ from django.db import models
 
 # Create your models here.
 
-class Link(models.Model):
-    """
-    A Model to store a tweet link in case a tweet has more than one link
-    """
-    tweet_link = models.URLField(max_length=200)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        verbose_name_plural = "Links"
-
 
 class Tip(models.Model):
     """
     A Model to store tweet details from Daily Python Tip
     """
-    tweep = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tips')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tips')
     python_tip = models.CharField(max_length=280)
-    link = models.ForeignKey(Link, on_delete=models.CASCADE,
-                             related_name='tips_link')  # e.g logging = Tip.objects.get(name='logging') ->
-    # logging.tips_link.all()
     posted_by = models.CharField(max_length=20)
     is_published = models.BooleanField(default=True)
     likes = models.IntegerField(blank=True, null=True)  # allow empty values for db and forms
@@ -34,7 +20,22 @@ class Tip(models.Model):
 
     class Meta:
         verbose_name_plural = "Tips"
-        ordering = ['-retweets', '-likes']
 
     def __str__(self):
         return self.python_tip
+
+
+class Link(models.Model):
+    """
+    A Model to store a tweet link in case a tweet has more than one link
+    """
+    tip = models.ForeignKey(Tip, on_delete=models.CASCADE, related_name='links')
+    tweet_link = models.URLField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = "Links"
+
+    def __str__(self):
+        return self.tweet_link
