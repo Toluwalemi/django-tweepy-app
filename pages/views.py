336050ae82db5dcc5c15c3a6a7763d30db2558d3
t.oluwadare <t.oluwadare@innovectives.com>
2021-03-04 04:44:48 +0100

    feature: add twitter verify credentials function

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+import os
+
+import tweepy
 from django.views.generic import TemplateView
+
+consumer_key = os.environ['TWITTER_API_KEY']
+consumer_secret = os.environ['TWITTER_SECRET_KEY']
+oauth_access_token = os.environ['TWITTER_ACCESS_TOKEN']
+oauth_access_token_secret = os.environ['TWITTER_ACCESS_TOKEN_SECRET']
+
+
+def verify_twitter_credentials(auth):
+    auth = tweepy.OAuthHandler(str(consumer_key), str(consumer_secret))
+    auth.set_access_token(str(oauth_access_token), str(oauth_access_token_secret))
+    api = tweepy.API(auth)
+    try:
+        api.verify_credentials()
+        return "Authentication OK"
+    except Exception:
+        return "Error during authentication"
 
 
 # Create your views here.
