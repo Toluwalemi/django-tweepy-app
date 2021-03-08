db51463b4e2f7cf201b74264aa4e0b35c63b7c54
t.oluwadare <t.oluwadare@innovectives.com>
2021-03-08 23:45:49 +0100

    feature: add dashboard url

--- pages/urls.py
+++ pages/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 
-from .views import Home  # new
+from .views import Home, show_doc_info  # new
 
 app_name = 'pages'
 
 urlpatterns = [
     # path('accounts/', include('allauth.urls')),
     path('', Home.as_view(), name="home"),
+    path('dashboard/', show_doc_info, name="some_home"),
 ]
