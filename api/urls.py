e836c3e8778923581662986f8a0a7222f8124427
t.oluwadare <t.oluwadare@innovectives.com>
2021-03-09 02:01:04 +0100

    feature: add endpoint for create view

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import TipAPIView
+from .views import TipAPIView, DetailTip, PostTip
 
 urlpatterns = [
+    path('<int:pk>/', DetailTip.as_view()),
     path('', TipAPIView.as_view()),
+    path('create/', PostTip.as_view()),
 ]
